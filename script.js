b49c177305b41db3b74458da847d00705f0d7544
Francesco <fviol000@gmail.com>
2025-02-20 12:07:09 +0100

    Aggiunta nuovi file 1

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log ("file esterno");
+
+// in base a determinati eventi (click, hover. ecc)
+// posso far eseguire del codice e cambiare la UI
+function changeText(){
+    let test123 = document.getElementById("titolo");
+    test123.textContent = "Elemento cliccato!";
+    }
+
+    console.log("file esterno");
+
+    // in base a determinati eventi (click, hover, ecc)
+    // posso far eseguire del codice e cambiare la UI
+
+ //    function changeText() {
+ //     let paragraph = document.getElementById("paragrafo");
+ //   }   
